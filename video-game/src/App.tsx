b18793f16ba422eb1093b2c02fd5071a00b49208
Catherin2 <cayeronwi@gmail.com>
2025-06-28 00:54:34 +0000

    $ git commit -S -m "app commit"

--- video-game/src/App.tsx
+++ video-game/src/App.tsx
@@ -1,10 +1,20 @@
-import { Button, ButtonGroup } from "@chakra-ui/react"
+import { Grid, GridItem, Show } from "@chakra-ui/react"
 
 function App() {
 
 
   return (
-    <Button>Button</Button>
+    <Grid templateAreas={
+      {
+        base: '"nav" "main"',
+        lg: '"nav nav" "aside main"'  //1024px
+      }}>
+      <GridItem area='nav' bg='red'>Nav</GridItem>
+
+      <GridItem area='aside' bg='gold'>Aside</GridItem>
+
+      <GridItem area='main' bg='skyblue'>Main</GridItem>
+    </Grid>
   )
 }
 
--- video-game/src/components/ui/tooltip.tsx
+++ video-game/src/components/ui/tooltip.tsx
@@ -1,6 +1,7 @@
 import { Tooltip as ChakraTooltip, Portal } from "@chakra-ui/react"
 import * as React from "react"
 
+
 export interface TooltipProps extends ChakraTooltip.RootProps {
   showArrow?: boolean
   portalled?: boolean
