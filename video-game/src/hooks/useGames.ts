7ab13b804dde21aed30ae52450fa7d2a37d57e06
Catherin2 <cayeronwi@gmail.com>
2025-07-01 16:59:49 +0000

    data hook

--- video-game/src/hooks/useGames.ts
+++ video-game/src/hooks/useGames.ts
@@ -1,6 +1,4 @@
-import React, { useEffect, useState } from 'react'
-import apiClient from '../services/api-client';
-import { CanceledError } from 'axios';
+import useData from './useData';
 
 
 export interface Game {
@@ -9,37 +7,10 @@ export interface Game {
     background_image: string;
     metacritic: number;
 }
-interface FetchGames {
-    count: number;
-    results: Game[];
-}
 
-const useGames = () => {
 
-    const [games, setGames] = useState<Game[]>([]);
-    const [error, setError] = useState('');
-    const [isLoading, setLoading] = useState(false);
+const useGames = () => useData<Game>('/games');
 
-    useEffect(() => {
-        const controller = new AbortController();
-        setLoading(true);
-        apiClient
-            .get<FetchGames>('/games', { signal: controller.signal })
-            .then((res) => {
-                setGames(res.data.results);
-                setLoading(false);
-            })
-            .catch(err => {
-                if (err instanceof CanceledError) return;
-                setError(err.message)
-                setLoading(false);
-            });
 
-        return () => controller.abort();
-    }, []);
-    return (
-        { games, error, isLoading }
-    )
-}
 
 export default useGames
