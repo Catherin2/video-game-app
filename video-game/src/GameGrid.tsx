bfb649aa40dbd9280e7bd877bb9f89a1d7085ea0
Catherin2 <cayeronwi@gmail.com>
2025-07-04 12:31:16 +0000

    gamegrid edit

--- video-game/src/GameGrid.tsx
+++ video-game/src/GameGrid.tsx
@@ -3,23 +3,26 @@ import useGames from './hooks/useGames'
 import GameCard from './components/GameCard';
 import CardSkeleton from './components/CardSkeleton';
 import GameCardContainer from './components/GameCardContainer';
+import { Genre } from './hooks/useGenres';
 
+interface Props {
+    filteredGenre: Genre | null
+}
 
-
-const GameGrid = () => {
-    const { data, error, isLoading } = useGames();
+const GameGrid = ({ filteredGenre }: Props) => {
+    const { data, error, isLoading } = useGames(filteredGenre);
     const skeletons = [1, 2, 3, 4, 5, 6];
     return (
         <>
             {error && <Text>{error}</Text>}
             <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} padding='10px' gap='4'>
                 {isLoading && skeletons.map(skeleton =>
-                    <GameCardContainer>
-                        <CardSkeleton key={skeleton} />
+                    <GameCardContainer key={skeleton} >
+                        <CardSkeleton />
                     </GameCardContainer>)}
                 {data.map((game) => (
-                    <GameCardContainer>
-                        <GameCard key={game.id} game={game} />
+                    <GameCardContainer key={game.id}>
+                        <GameCard game={game} />
                     </GameCardContainer>))}
             </SimpleGrid>
         </>
